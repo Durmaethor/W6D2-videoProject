a701dcdbeabe5ef80db4dc942b88e03471f3d807
Ryan Kramer <rkramer613@gmail.com>
2017-02-09 18:09:23 -0700

    add more and styles

--- js/ctrl.js
+++ js/ctrl.js
@@ -2,6 +2,12 @@ angular.module('myApp').controller('myCtrl', function($scope){
 
     $scope.test = "Booger"
 
+    /* {
 
+        name: 'Luke Skywalker',
+        niceScore: (0-100),
+        naughtyScore: (0-100)
+        
+        }
     
 });
